e7d1e88932b55c9d7bdd89f2c66805e9895f9db0
Eduardo\Eduardo Oliveira <eduardo.olivei@outlook.com>
2025-02-05 19:18:28 -0400

    [UP COMAND START]

--- .nuxt/dist/server/server.mjs
+++ .nuxt/dist/server/server.mjs
@@ -1,1 +1,6 @@
-export { default } from "file:///C:/Users/Eduardo%20Oliveira/Documents/dev_projetos/oracle-to-postgres/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
+const entrySpa = () => {
+};
+export {
+  entrySpa as default
+};
+//# sourceMappingURL=server.mjs.map
